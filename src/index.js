41b975c1bcfdbe6da3af267e017a7df56b79e3d5
sashaivanovaPro <sashaivanova.pro@gmail.com>
2023-02-07 18:44:09 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = Math.abs(n).toPrecision();
+    let reverseStr = str.split("").reverse().join("")
+    let reverseNum = Number(reverseStr);
+    return reverseNum
 }
+
+
